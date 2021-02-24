6f23b344029d23edc4d3e0a82d4ec158e7c4a72c
AtmoStone <a.l.e.x.e.y.b@yandex.ru>
2021-02-24 10:12:23 +0300

    Added ability to create workers

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from apps.company.views import CreateWorkerProfile
+
 urlpatterns = [
+    path('addworker/', CreateWorkerProfile.as_view()),
 
 ]
--- apps/company/views.py
+++ apps/company/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
 
-# Create your views here.
+from apps.company.forms import WorkerProfileForm
+from apps.company.models import WorkerProfile, Company
+
+
+class CreateWorkerProfile(LoginRequiredMixin, CreateView):
+    model = WorkerProfile
+    template_name = 'company/form.html'
+    form_class = WorkerProfileForm
+    login_url = 'login/'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.company = Company.objects.get(name=WorkerProfile.objects.get(user=self.request.user).company)
+        return super().form_valid(form)
